dfdbe54652eeafb8a6e113f487f9433770d2890f
Aaron Essen <essenaaron@gmail.com>
2024-08-06 23:20:11 +0200

    Unötiger Text

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,6 @@
-﻿//Test Programm test
+﻿//Test Programm
 string name;
 Console.WriteLine("Hello World");
 Console.Write("Benutzernamen eingeben: ");
 name = Console.ReadLine();
 Console.WriteLine("Hello " + name);
-//he
