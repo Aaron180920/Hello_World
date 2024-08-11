6971979dccc3e32a88e9dc499d74b27a8937a46e
Aaron Essen <essenaaron@gmail.com>
2024-08-11 19:10:12 +0200

    Späte Initialisierung

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,5 @@
 ﻿//Test Programm
-string name;
 Console.WriteLine("Hello World");
 Console.Write("Benutzernamen eingeben: ");
-name = Console.ReadLine();
+var name = Console.ReadLine();
 Console.WriteLine("Hello " + name);
