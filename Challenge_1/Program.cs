ed8bf975f400103a5812cb0612c73d7a072dc7fc
Aaron Essen <essenaaron@gmail.com>
2024-09-18 21:29:47 +0200

    Challenge 1

---
+++ Challenge_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Challenge 1
+Console.Write("Gebe Ergebnis ein: ");
+double ergebnis = Convert.ToDouble(Console.ReadLine());
+if(ergebnis > 1 || ergebnis < 0.6)
+{
+    Console.WriteLine("sould be F");
+}
+else if( ergebnis >= 0.9)
+{
+    Console.WriteLine("sould be A");
+}
+else if (ergebnis >= 0.8)
+{
+    Console.WriteLine("sould be B");
+}
+else if (ergebnis >= 0.7)
+{
+    Console.WriteLine("sould be C");
+}
+else if (ergebnis >= 0.6)
+{
+    Console.WriteLine("sould be D");
+}
